0cfbfab3cf7b7df8434bede3155ca205abded096
Anthony Scola <anthonyscola@outlook.com>
2023-11-21 16:20:04 -0500

    Implement visual improvements

--- Form1.cs
+++ Form1.cs
@@ -13,8 +13,18 @@ namespace my_ost
 {
     public partial class Form1 : Form
     {
+        public class SongMetadata
+        {
+            public string SongImg { get; set; }
+            public string SongName { get; set; }
+            public string Artist { get; set; }
+            public string SongTags { get; set; }
+            public string SongDuration { get; set; }
+        }
+
         WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
-        List<string> mp3Locations = new List<string>();
+        List<string> mp3LocationList = new List<string>();
+        List<SongMetadata> mp3MetadataList = new List<SongMetadata>();
 
         public Form1()
         {
@@ -23,12 +33,21 @@ namespace my_ost
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            mp3MetadataList.Add(new SongMetadata
+            { 
+                SongImg = "",
+                SongName = "Test",
+                Artist = "Artist",
+                SongTags = "Tagzzz",
+                SongDuration = "bizzar"
+            });
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = mp3MetadataList;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            foreach (string mp3 in mp3Locations)
+            foreach (string mp3 in mp3LocationList)
             {
                 player.URL = mp3;
                 player.controls.play();
@@ -43,13 +62,19 @@ namespace my_ost
         private void openToolStripButton_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "MP3 Files|*.mp3";
-            openFileDialog.Title = "Select an MP3 File";
+            openFileDialog.Filter = "MP3 Files|*.mp3|OGG Files|*.ogg|All Files|*.*";
+            openFileDialog.Title = "Select an Audio File";
 
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                mp3Locations.Add(openFileDialog.FileName);
+                mp3LocationList.Add(openFileDialog.FileName);
+
             }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
